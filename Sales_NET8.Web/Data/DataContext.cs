325c111b9cade4d22d4c918e70fe8c78e74de1a0
PedroMarPT <mentesserias@gmail.com>
2024-09-02 19:12:22 +0100

    Countries partial views

--- Sales_NET8.Web/Data/DataContext.cs
+++ Sales_NET8.Web/Data/DataContext.cs
@@ -1,20 +1,25 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Sales_NET8.Web.Data.Entities;
 using Sales_NET8.Web.Entities;
 
 namespace Sales_NET8.Web.Data
 {
     public class DataContext : DbContext
     {
-        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
 
+        }
         public DbSet<Country> Countries { get; set; }
+        public DbSet<Category> Categories { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-            modelBuilder.Entity<Country>()
-                .HasIndex(c => c.Name)
-                .IsUnique();
+
+            modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();
+
+            modelBuilder.Entity<Category>().HasIndex(c => c.Name).IsUnique();
         }
     }
 }
--- Sales_NET8.Web/Program.cs
+++ Sales_NET8.Web/Program.cs
@@ -11,6 +11,10 @@ namespace Sales_NET8.Web
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            //Add runtime compilation
+            builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+
             builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=LocalConnection"));
             //builder.Services.AddDbContext<DataContext>(options =>
             //options.UseSqlServer(builder.Configuration.GetConnectionString("LocalConnection")));
